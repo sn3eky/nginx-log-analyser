69b34c43a23cfaed0f4b8d7559121ccb50c746f0
Sneeky <lsimon31@protonmail.com>
2024-11-12 14:55:33 +0100

    Update log-analyser.sh - Modifying structure adding top5_IP function

--- log-analyser.sh
+++ log-analyser.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-read_file(){ 
+top5_IP(){
     log_file='nginx-access.log'
-    while read line
-    do
-        echo "$line"
-    done < "$log_file"
+    printf "Top 5 IP with the most requests \n"
+    # On affiche la première colonne du fichier logs, on trie dans un premier temps les adresses IP puis on les compte puis on retrie en fonction du nombre de fois où elles apparaissent, puis on affiche le top 5
+    # La boucle while va lire les deux colonnes count et ip puis les afficher dans l'ordre voulu (IP-COUNT)
+    awk '{print $1}' "$log_file" | sort | uniq -c | sort -nr | head -n 5 | while read -r count ip; do
+        printf "%s - %s requests\n" "$ip" "$count"
+    done
 }
+top5_IP
